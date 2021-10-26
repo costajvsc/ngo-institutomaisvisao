fedf3bd5feee001af0e2e5ca8df2d0a1ae07235f
Joao Victor <Joao Victor@DESKTOP-MAD>
2021-10-26 07:59:07 -0300

    fix: ajustando visualização de dados

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,27 +7,30 @@
         <div class="row mt-2 mb-2">
             @foreach($solicitacoes as $s)
                 <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 mb-4">
-                    <div class="card text-center border-secondary">
-                        <div class="card-body">
-                            <div class="d-flex justify-content-between mb-3">
-                                <div class="d-flex align-items-center">
-                                    <span class="badge rounded-pill bg-secondary">{{$s->status}}</span>
+                    <div class="card text-center border-secondary h-100">
+                        <div class="card-body d-flex flex-column justify-content-between">
+                            <div>
+                                <div class="d-flex justify-content-between">
+                                    <div class="d-flex align-items-center">
+                                        <span class="badge rounded-pill bg-secondary">{{$s->status}}</span>
+                                    </div>
+                                    <div class="text-end">
+                                        <span class="fw-light">{{date_format(new DateTime($s->created_at), 'd/m/Y')}}</span> <br>
+                                        <span class="fw-light">{{$s->procedimento}}</span>
+                                    </div>
                                 </div>
-                                <div>
-                                    <span class="fw-light">{{date_format(new DateTime($s->created_at), 'd/m/Y')}}</span> <br>
-                                    <span class="fw-light">{{$s->procedimento}}</span>
+                                <div class="d-flex justify-content-between">
+                                    <h5 class="card-title">{{$s->nome_paciente}}</h5>
+                                    <p class="card-text fw-light">{{$s->sobrenome_paciente}}</p>
                                 </div>
-                            </div>
-                            <div class="d-flex justify-content-between">
-                                <h5 class="card-title">{{$s->nome_paciente}}</h5>
-                                <p class="card-text fw-light">{{$s->sobrenome_paciente}}</p>
-                            </div>
 
-                            <div class="d-flex justify-content-between mb-4">
-                                <a href="tel:{{$s->telefone_fixo}}" class="fw-light">{{"(".substr($s->telefone_fixo,0,2).") ".substr($s->telefone_fixo,2,-4)."-".substr($s->telefone_fixo,-4)}}</a>
-                                <a href="tel:{{$s->telefone_fixo}}" class="fw-light">{{"(".substr($s->telefone_celular,0,2).") ".substr($s->telefone_celular,2,-4)."-".substr($s->telefone_celular,-4)}}</a>
+                                <div class="d-flex justify-content-between mb-4">
+                                    <a href="tel:{{$s->telefone_fixo}}" class="fw-light">{{"(".substr($s->telefone_fixo,0,2).") ".substr($s->telefone_fixo,2,-4)."-".substr($s->telefone_fixo,-4)}}</a>
+                                    <a href="tel:{{$s->telefone_fixo}}" class="fw-light">{{"(".substr($s->telefone_celular,0,2).") ".substr($s->telefone_celular,2,-4)."-".substr($s->telefone_celular,-4)}}</a>
+                                </div>
                             </div>
                             <p class="card-text mb-4">{{$s->observacao}}</p>
+
                             <a href="/solicitacao/{{$s->id}}" class="btn btn-outline-secondary"> <i class="fas fa-file-medical"></i> Acessar solicitação </a>
                         </div>
                     </div>
